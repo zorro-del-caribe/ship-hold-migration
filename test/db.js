0b7ef96b28cd99b020de7925c3c7b8ff5b366331
lorenzofox3 <laurent34azerty@gmail.com>
2018-04-16 10:13:30 -0400

    fix ci

--- test/db.js
+++ test/db.js
@@ -1,5 +1,4 @@
 module.exports = {
-	host: process.env.POSTGRES_HOST || '127.0.0.1',
 	user: process.env.POSTGRES_USER || 'docker',
 	password: process.env.POSTGRES_PASSWORD || 'docker',
 	database: process.env.POSTGRES_DB || 'ship-hold-migrations-test'
